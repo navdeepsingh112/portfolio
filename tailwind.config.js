9c999063ffef44d75f25c7ebd0e76a3ae695a3ef
navdeepsingh112 <niosgurpreet@gmail.com>
2025-08-13 12:28:02 +0530

    portfolio update

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in-out',
+        'bounce': 'bounce 2s infinite',
+      },
+    },
+  },
+  plugins: [],
+}
